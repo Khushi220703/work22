38316984231bf57b2b42ecc21a969cef5b14dcd6
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2024-04-10 20:00:37 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const nodeMailer = require("nodemailer");
 
 
 const PORT = process.env.PORT || 5000;
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://work22-p3i1.vercel.app');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 const corsOptions =
 {
