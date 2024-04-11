94fc56aecd39ff6acf9d2cef2d14587f8b387a36
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2024-04-11 17:36:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions =
 {
-    origin:['https://work22-p3i1.vercel.app/'],
+    origin:"*",
     Credential:true,
    
     optionSucessStatus:200
